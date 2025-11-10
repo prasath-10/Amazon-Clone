e627da6f20a1e2bc4c42b6833ad8cb78b000309f
PRIYADHARSHINI-SHANMUGAM <priyadharshinis.20cse@kongu.edu>
2025-11-10 12:24:39 +0530

    save cart to local storage

--- backend/cart.js
+++ backend/cart.js
@@ -1,10 +1,19 @@
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart=[{
    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
    quantity:2,
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1,
 }];
+}
+
+
+function savetostorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
  export function addToCart (cartId){
  let matchingitem;
          cart.forEach((item)=>{
@@ -21,6 +30,7 @@ export let cart=[{
               quantity:1
             });
          }
+         savetostorage();
         }
 
 export function removefromcart(productId){
@@ -31,5 +41,6 @@ export function removefromcart(productId){
       }
      });
      cart=newcart;
+     savetostorage();
 }
   
